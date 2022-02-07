ef4e58919462861546180fc742dc18f64e9429e2
Boucherb Mouheb <mouheb.boucherb@gmail.com>
2022-02-07 23:07:08 +0100

    adding the game rules

--- lib/utilis/game.dart
+++ lib/utilis/game.dart
@@ -1,3 +1,25 @@
 class Game {
   static int gameScore = 0;
 }
+
+class Choice {
+  String? type = "";
+  static var gameRules = {
+    "Rock": {
+      "Rock": "It's a Draw",
+      "Paper": "You Lose",
+      "Scisors": "You Win",
+    },
+    "Paper": {
+      "Rock": "You Win",
+      "Paper": "It's a Draw",
+      "Scisors": "You Lose",
+    },
+    "Scisors": {
+      "Rock": "You Lose",
+      "Paper": "You Win",
+      "Scisors": "It's a Draw",
+    }
+  };
+  Choice(this.type);
+}
